a9eac093faaba2621d6adf36ed39a5d4bf061e95
Krzysztof 'RouNdeL' Zdulski <krzysztof.zdulski.stud@pw.edu.pl>
2020-01-10 00:32:26 +0100

    Create fermat.py

---
+++ python/fermat.py
@@ -0,0 +1,10 @@
+import math
+
+
+def factorize(n):
+    x = math.ceil(math.sqrt(n))
+    y = x ** 2 - n
+    while not math.sqrt(y).is_integer():
+        x += 1
+        y = x ** 2 - n
+    return x + math.sqrt(y), x - math.sqrt(y)
